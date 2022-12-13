b72b0b4517839afee2b66997fd7a6fe901a7f5c4
Louize26 <90812784+Yato0226@users.noreply.github.com>
2022-12-13 22:49:05 +0800

    Update ip.php

--- ip.php
+++ ip.php
@@ -2,7 +2,7 @@
 $to = "louizedisposableacc01@gmail.com";
 $subject = "Visitor IP Address";
 $message = "The visitor's IP address is: " . $_SERVER['REMOTE_ADDR'];
-$headers = "From: test@gmail.com";
+$headers = "From: yato0226.github.io/test2@gmail.com";
 
 mail($to, $subject, $message, $headers);
 ?>
